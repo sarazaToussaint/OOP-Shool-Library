75e3735c43abd819a54fba5f945ec78859095583
sarazaToussaint <tmsaraza@gmail.com>
2022-07-26 15:25:38 +0200

    Fix linters errors

--- person.rb
+++ person.rb
@@ -1,20 +1,20 @@
 class Person
-  attr_reader: id
-  attr_accessor : name, : age
+  attr_reader :id
+  attr_accessor :name, :age
 
-  def initialize(name = 'Unknown', age, parent_permission = 'true')
+  def initialize(age, name = 'Unknown', _parent_permission = 'true')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
   end
-  
-  def is_of_age?
+
+  def of_age?
     @age >= 18
   end
   private :is_of_age?
 
   def can_use_services?
-    is_of_age? || @parent_permission
+    of_age? || @parent_permission
   end
   public :can_use_services?
 end
--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require_relative './person'
 
 class Student < Person
-    def initialize(name = 'Unknown', age, parent_permission = 'true', classroom)
-        super(age, name, parent_permission: parent_permission)
-        @classroom = classroom
-      end
-    
-    def play_hook
-      "¯\(ツ)/¯"
-    end      
+  def initialize(age, classroom, name = 'Unknown', parent_permission = 'true')
+    super(age, name, parent_permission: parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hook
+    "¯\(ツ)/¯"
+  end
 end
