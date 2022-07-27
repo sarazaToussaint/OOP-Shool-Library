ca49145bb128a7a9e092e78fa27dd24c4f291ab8
sarazaToussaint <tmsaraza@gmail.com>
2022-07-27 12:25:25 +0200

    Turn a Person class to Nameable class

--- person.rb
+++ person.rb
@@ -1,17 +1,25 @@
-class Person
+require_relative './nameable'
+
+class Person < Nameable
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = 'Unknown', _parent_permission = 'true')
+  def initialize(age, name = 'Unknown', parent_permission : true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
+    @parent_permission = parent_permission
+    super()
   end
 
   def of_age?
     @age >= 18
   end
   private :is_of_age?
+
+  def correct_name
+    @name
+  end
 
   def can_use_services?
     of_age? || @parent_permission
