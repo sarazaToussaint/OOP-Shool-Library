3edee3f89c99922a2da03d538521f4fee3d54b0c
sarazaToussaint <tmsaraza@gmail.com>
2022-07-27 18:06:15 +0200

    Add Rental class and use it as intermediate between Person and Book classes

---
+++ rental.rb
@@ -0,0 +1,12 @@
+class Rental
+  attr_accessor :date, :book, :person
+
+  initialize(date, book, person)
+    @date = date
+    @book = book
+    book.rentals << self
+
+    @person = person
+    person.rentals << self
+  end   
+end    
