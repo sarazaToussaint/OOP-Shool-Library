fb7472a717bf4e5bde48b8e0f9d7fb1b7ded73db
sarazaToussaint <tmsaraza@gmail.com>
2022-07-28 17:25:16 +0200

    Add a method to convert date

--- app.rb
+++ app.rb
@@ -122,9 +122,15 @@ end
 def list_rentals
   puts 'Select id of any person'
   @people.each { |i| puts "id: #{i.id}, Person: #{i.name}" }
-    print 'Person id: '
-    person_id = gets.chomp
+  print 'Person id: '
+  person_id = gets.chomp
   @rentals.each do |i|
     puts "Date: #{i.date}, Book: '#{i.book.title}' by #{i.book.author}" if i.person.id.to_i == person_id.to_i
+  end
+end
+
+# Convert date
+def convert_date(str)
+  Date.parse(str)
   end
 end
