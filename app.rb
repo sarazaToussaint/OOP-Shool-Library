8a5258fadd7ea7506ddeaf75e07a80ef23c12c5e
sarazaToussaint <tmsaraza@gmail.com>
2022-07-29 19:17:39 +0200

    Delete unused code

--- app.rb
+++ app.rb
@@ -130,9 +130,4 @@ class App
       puts "Date: #{i.date}, Book: '#{i.book.title}' by #{i.book.author}" if i.person.id.to_i == person_id.to_i
     end
   end
-
-  # Convert date
-  # def convert_date(str)
-  #   Date.parse(str)
-  # end
 end
